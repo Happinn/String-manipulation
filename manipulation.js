346af124f36bedc146ba13dbf4d6d020f4e20d65
Happinn <josephay20@gmail.com>
2024-01-16 13:45:47 +0100

    add changes

--- manipulation.js
+++ manipulation.js
@@ -1,33 +1,53 @@
-const label = document.querySelector('h1')
-const fullname = label.textContent
+
+let fullname = "Ogunsanya ayodele joseph"
 
 //const lists = document.getElementsByTagName('li')
-const lists = document.querySelectorAll('ol li')
+// const lists = document.querySelectorAll('ol li')
 
 //length of a string
-lists[0].textContent += fullname.length
+// lists[0].textContent += fullname.length
+console.log("The length of the name", fullname.length)  
 
 //touppercase method
-lists[1].innerHTML += `<strong>${fullname.toUpperCase()}</strong>`
+// lists[1].innerHTML += `<strong>${fullname.toUpperCase()}</strong>`
+console.log("To uppercase", fullname.toUpperCase())
 
-lists[2].innerHTML += `<strong>${fullname.toLowerCase()}</strong>`
+//touppercase method
+// lists[2].innerHTML += `<strong>${fullname.toLowerCase()}</strong>`
+console.log("To lowercase", fullname.toLowerCase())
 
 //charAt method
-lists[3].textContent += `character at index "8": ` + fullname.charAt(8).toUpperCase()
+// lists[3].textContent += `character at index "8": ` + fullname.charAt(8).toUpperCase()
+console.log("Character at index '8' is", fullname.charAt(8))
 
 //substring method
-lists[4].innerHTML += `extract a substring from the original string "d": ` + fullname.substring('d')
+// lists[4].innerHTML += `extract a substring from the original string "d": ` + fullname.substring(1,7)
+console.log("replace substring from index 1 to 8", fullname.substring(1, 8))
+
+//updated string
+let updatedstring = fullname.replace("e", "a")
+console.log("updated string:", updatedstring)
 
 //replace
-lists[5].innerHTML += `replace "joseph" with "leonard": ` + fullname.replace('joseph', 'leonard')
+// lists[5].innerHTML += `replace "joseph" with "leonard": ` + fullname.replace('joseph', 'leonard')
+console.log("Replace 'joseph' with 'leonard' is", fullname.replace('joseph', 'leonard'))
 
 //indexOf
-lists[6].innerHTML += `The index of letter "l" is: ${fullname.indexOf('l')}`
+// lists[6].innerHTML += `The index of letter "l" is: ${fullname.indexOf('l')}`
+// console.log("The index of letter 'l' is", fullname.indexOf('l'))
+let position = fullname.indexOf("l")
+console.log("position of 'e' :", position)
 
 
-lists[7].textContent = 'ayodele'
+// lists[7].textContent = 'ayodele'
 
 //concat
-lists[8].textContent += `The fullname starts with: ` .concat(ogunsanya, 'welcomeback')
+// lists[8].textContent += `The fullname starts with: ` .concat(ogunsanya, 'welcomeback')
+let greeting = "Welcome Back, "
+let combineResult = greeting.concat(fullname)
+console.log("combine result:", combineResult)
 
-lists[9].innerHTML = `if the length is greater then: ${fullname.length}`
+let isLengthGreaterThanSeven = fullname.length > 7
+console.log("is the length greater than 7?", isLengthGreaterThanSeven)
+
+// lists[9].innerHTML = `if the length is greater then: ${fullname.length}`
--- variable.js
+++ variable.js
@@ -4,17 +4,24 @@ let x= 7
 let y = 4
 let z = 1
 
-let d = x + y
-variable[0].innerHTML = 'x + y is: ' + d
+// let d = x + y
+// variable[0].innerHTML = 'x + y is: ' + d
 
-let e = y - z
-variable[1].innerHTML = 'y - z is: ' + e
+// let e = y - z
+// variable[1].innerHTML = 'y - z is: ' + e
 
-let f = x * z
-variable[2].innerHTML = 'x * z is: ' + f
+// let f = x * z
+// variable[2].innerHTML = 'x * z is: ' + f
 
-let g = z/y
-variable[3].innerHTML = 'z/y is: ' + g
+// let g = z/y
+// variable[3].innerHTML = 'z/y is: ' + g
 
-let h = x % y
-variable[4].innerHTML = 'x % y is: ' + h
+// let h = x % y
+// variable[4].innerHTML = 'x % y is: ' + h
+
+console.log(addition_result = x + y)
+console.log(subtraction_result = y - z)
+console.log(multiplication_result = x * z)
+console.log(division_result = z/y)
+console.log(modulus_result = x % y)
+
